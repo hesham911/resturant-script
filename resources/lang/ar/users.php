11d10d3e81c2d052d5d8d48cd4e5d81ff28a49d1
hesham.hanafi911@gmail.com <hesham.hanafi911@gmail.com>
2021-04-23 02:08:15 +0200

    add users translate file

---
+++ resources/lang/ar/users.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    'employees' =>[
+        'titles'            => [
+            'index'                 => ' الموظفين ',
+            'create'                => ' إضافة الموظف ',
+            'subcreate'             => 'إضافة موظف جديدة',
+            'edit'                  => 'تعديل  الموظف  ',
+            'show'                  => ' عرض  الموظف  ',
+        ],
+        'placeholder'       => [
+            'name'                       => ' الاسم الموظف ',
+            'email'                      => ' الإيميل الدخول للموظف ',
+            'password'                   => ' كلمة السر ',
+            'confirm_password'           => ' تأكيد كلمة السر ',
+            'types_employees'            => ' طبيعة عمل الموظف ',
+        ],
+        'massages'          => [
+            'created_successfully'   => 'تم  تسجيل  الموظف   بنجاح',
+            'updated_successfully'   => 'تم تعديل  الموظف بنجاح',
+            'deleted_check'          => 'هل تريد حقا حذف هذة الموظف',
+            'deleted_successfully'   => 'تم حذف  الموظف بنجاح',
+            'error_successfully'     => 'حدث خطأ من فضلك راجع البيانات المطلوبة',
+        ],
+        'name'                       => ' الاسم ',
+        'email'                      => ' الإيميل ',
+        'password'                   => ' كلمة السر ',
+        'confirm_password'           => ' تأكيد كلمة السر ',
+        'types_employees'            => ' الوظيفة ',
+        'status'                     => ' حالة الموظف ',
+    ],
+];
