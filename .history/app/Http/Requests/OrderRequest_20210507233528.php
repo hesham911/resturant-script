6ed5493d7888bf7beae98c94831118469e68e7d3
Mohammed Basha <mohamedbasha275@gmail.com>
2021-05-08 19:42:18 +0200

    editing order and add qunatity of one product

---
+++ .history/app/Http/Requests/OrderRequest_20210507233528.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class OrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+           // 'client_id'  =>   'required',
+            'category_id' =>   'required',
+            'type' =>   'required|numeric',
+            'group_a[qua]' =>   'required|numeric',
+            'table_id'   =>'required_if:type,==,0',
+            //'products'   =>'required',
+        ];
+    }
+    public function attributes()
+    {
+        return [
+            'client_id'      =>__('orders.client_id'),
+            'category_id'     =>__('orders.category_id'),
+            'table_id'     =>__('orders.table_id'),
+            'type'     =>__('orders.order_type'),
+            'products'     =>__('orders.products'),
+        ];
+    }
+}
