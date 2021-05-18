f19006980e74a765087a2c2a859c8335de2f526f
hesham.hanafi911@gmail.com <hesham.hanafi911@gmail.com>
2021-05-19 01:51:23 +0200

    add translate files and validation of roles module

---
+++ resources/lang/ar/roles.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'titles'            => [
+        'index'                 => ' الصلاحيات ',
+        'create'                => ' إضافة صلاحية ',
+        'subcreate'             => 'إضافة صلاحية جديدة',
+        'edit'                  => 'تعديل  الصلاحية  ',
+        'show'                  => ' عرض  الصلاحية  ',
+    ],
+    'placeholder'       => [
+        'name'                  => ' اسم الصلاحية ',
+        'permissions'           => ' أذونات ',
+    ],
+    'massages'          => [
+        'created_successfully'   => 'تم  تسجيل  الصلاحية   بنجاح',
+        'updated_successfully'   => 'تم تعديل  الصلاحية بنجاح',
+        'deleted_check'          => 'هل تريد حقا حذف هذة الصلاحية',
+        'deleted_successfully'   => 'تم حذف  الصلاحية بنجاح',
+        'error_successfully'     => 'حدث خطأ من فضلك راجع البيانات المطلوبة',
+    ],
+    'name'                       => ' الاسم ',
+    'permissions'                => ' أذونات ',
+];
