c87a87104059913ecb369c46300daba58893b261
hesham.hanafi911@gmail.com <hesham.hanafi911@gmail.com>
2021-05-19 01:55:06 +0200

    add link of create on roles index page && add select2 on create roles

--- resources/views/admin/roles/create.blade.php
+++ resources/views/admin/roles/create.blade.php
@@ -1,20 +1,22 @@
 @extends('layouts.app')
 @section('title')
-    {{__('geo.zones.titles.create')}}
+    {{__('roles.titles.create')}}
 @endsection
 @section('head')
     <!-- Prism -->
     <link rel="stylesheet" href="{{ url('vendors/prism/prism.css') }}" type="text/css">
+    <!-- select2 css -->
+    <link rel="stylesheet" href="{{url('vendors/select2/css/select2.min.css')}}" type="text/css">
 @endsection
 
 @section('content')
 
     <div class="page-header">
         <div>
-            <h3>{{__('geo.zones.titles.create')}}</h3>
+            <h3>{{__('roles.titles.create')}}</h3>
             @include('admin.partials.breadcrumb',[
                 'parent' => [
-                    'name' => __("geo.zones.titles.create"),
+                    'name' => __("roles.titles.create"),
                 ]
             ])
         </div>
@@ -41,18 +43,25 @@
                                 </div>
                             @endif
                             <h6 class="card-title">{{__('geo.zones.titles.subcreate')}}</h6>
-                            <form method="post" action="{{route('zones.store')}}" multiple>
+                            <form method="post" action="{{route('roles.store')}}" multiple>
                                 @CSRF
                                 <div class="form-group row">
-                                    <label for="name" class="col-sm-2 col-form-label">{{__('geo.zones.name')}}</label>
+                                    <label for="name" class="col-sm-2 col-form-label">{{__('roles.name')}}</label>
                                     <div class="col-sm-10">
-                                        <input type="text" name="name" value="{{old('name')}}" class="form-control" id="name" placeholder="{{__('geo.zones.placeholder.name')}}">
+                                        <input type="text" name="name" value="{{old('name')}}" class="form-control" id="name" placeholder="{{__('roles.placeholder.name')}}">
                                     </div>
                                 </div>
                                 <div class="form-group row">
-                                    <label for="price" class="col-sm-2 col-form-label">{{__('geo.zones.price')}}</label>
+                                    <label for="permission" class="col-sm-2 col-form-label">{{__('roles.permissions')}}</label>
                                     <div class="col-sm-10">
-                                        <input type="text" name="price" value="{{old('price')}}" class="form-control" id="price" placeholder="{{__('geo.zones.placeholder.price')}}">
+                                        <select name="permission[]" id="permission" class="roles-employees" multiple>
+                                            <option disabled >{{__('roles.placeholder.permissions')}}</option>
+                                            @if(count($permissions) > 0)
+                                                @foreach($permissions as $key => $permission)
+                                                    <option value="{{$permission->name}}" {{ (old('permission') == $permission->name) ? 'selected':'' }}>{{$permission->name}}</option>
+                                                @endforeach
+                                            @endif
+                                        </select>
                                     </div>
                                 </div>
                                 <div class="form-group row">
@@ -77,4 +86,12 @@
 
     <!-- Prism -->
     <script src="{{ url('vendors/prism/prism.js') }}"></script>
+    <script src="{{url('vendors/select2/js/select2.min.js')}}"></script>
+    <script>
+        $(document).ready(function () {
+            $('.roles-employees').select2({
+                placeholder: "إضافة الأذونات"
+            });
+        });
+    </script>
 @endsection
