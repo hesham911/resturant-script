d465f850173ebf33d76c2c58acab5b5d62b5a12e
hesham.hanafi911@gmail.com <hesham.hanafi911@gmail.com>
2021-06-06 00:11:58 +0200

    add scope end shift

--- app/Bank.php
+++ app/Bank.php
@@ -4,11 +4,28 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Builder;
 
 class Bank extends Model
 {
     use SoftDeletes;
-   protected $fillable=['name','opening_balance','notes'];
+   protected $fillable=['name','opening_balance','notes','type'];
+
+    static function type()
+    {
+        $array=
+            [
+                0         => 'كاشير',
+                1         => 'محاسب',
+
+            ];
+        return $array;
+    }
+    // get type
+    public function getTypeeAttribute()
+    {
+        return self::type()[$this->type];
+    }
 
     public function workPeriods()
     {
@@ -20,7 +37,15 @@ class Bank extends Model
         return $this->hasMany(BankTransaction::class,'bank_id');
     }
 
-    public function ScopeGetBankFromId($query,$id){
+    public function ScopeGetBankFromId(Builder $query,$id){
         return $query->findOrFail($id);
+    }
+
+    public function scopeGetBankCashierNotInWork( $query)
+    {
+        return $query->where('type','0')->doesntHave('workPeriods')
+            ->orWhereHas('workPeriods',function (Builder $q){
+            $q->where('status','!=',1);
+        });
     }
 }
--- app/WorkPeriod.php
+++ app/WorkPeriod.php
@@ -9,15 +9,90 @@ class WorkPeriod extends Model
 {
     use SoftDeletes;
 
-    protected $fillable=['user_id','bank_id','opening_balance','close_balance'];
+    /**
+     * @var array
+     */
+    protected $dates=[
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'date_start',
+        'date_end'
+    ];
+    /**
+     * @var array
+     */
+    protected $fillable=[
+        'user_id',
+        'bank_id',
+        'opening_balance',
+        'close_balance',
+        'date_start',
+        'date_end',
+        'status'
+    ];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function employee()
     {
         return $this->belongsTo(User::class,'user_id');
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function bank()
     {
         return $this->belongsTo(Bank::class,'bank_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function payments()
+    {
+        return $this->hasMany(Payment::class,'work_period_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function indirectexpenses()
+    {
+        return $this->hasMany(IndirectExpense::class,'work_period_id');
+    }
+
+    /**
+     * @param $query
+     * @param $id
+     * @return mixed
+     */
+    public function scopeGetAllIncome($query, $id)
+    {
+       $work = $this->findOrFail($id);
+       return $work->payments;
+    }
+
+    /**
+     * @param $query
+     * @param $id
+     * @return mixed
+     */
+    public function scopeGetAllOutCome($query, $id)
+    {
+        $work = $this->findOrFail($id);
+        return $work->indirectexpenses;
+    }
+
+    /**
+     * @param $query
+     * @param $id
+     * @return mixed
+     */
+    public function scopeGetCloseBalance($query, $id)
+    {
+        return $this->getAllIncome($id)->sum('total_price') - $this->getAllOutCome($id)->sum('amount');
     }
 }
